475f1c12ba8d8981013906631313a5a9794d9ba2
joisys <joisys@gmail.com>
2025-08-06 10:04:55 +0100

    Repository Generic Class Added

---
+++ StoreCard.Data/Interfaces/IRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace StoreCard.Data.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        DbSet<T> GetDbset();
+
+        IAsyncEnumerable<T> GetAsync(Expression<Func<T, bool>>? filter = null,
+            IEnumerable<Expression<Func<T, object>>>? includes = null,
+            bool asNoTracking = false);
+
+        IQueryable<T> GetQueryable(Expression<Func<T, bool>>? filter = null,
+            IEnumerable<Expression<Func<T, object>>>? includes = null,
+            bool asNoTracking = false);
+
+
+        Task<IEnumerable<T>> GetAllAsync();
+
+        Task<T> GetByIdAsync(int id);
+
+        Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task RemoveAsync(T entity);
+    }
+}
---
+++ StoreCard.Data/Repository.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
+using StoreCard.Data.Interfaces;
+using System.Linq.Expressions;
+
+namespace StoreCard.Data
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly DbSet<T> _dbSet;
+        protected readonly DbContext _context;
+        protected readonly IMemoryCache _memoryCache;
+
+        public Repository(DbContext context, IMemoryCache memoryCache)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            _dbSet = context.Set<T>();
+            _memoryCache = memoryCache ?? throw new ArgumentNullException(nameof(memoryCache));
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            return entity;
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public IAsyncEnumerable<T> GetAsync(Expression<Func<T, bool>>? filter = null, IEnumerable<Expression<Func<T, object>>>? includes = null, bool asNoTracking = false)
+        {
+            var dbset = GetQueryable(filter, includes, asNoTracking);
+            return dbset.AsAsyncEnumerable();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            var entity = await _dbSet.FindAsync(id);
+            if (entity == null)
+            {
+                throw new InvalidOperationException($"Entity of type {typeof(T).Name} with ID {id} not found.");
+            }
+            return entity!;
+        }
+
+        public DbSet<T> GetDbset()
+        {
+            return _dbSet;
+        }
+
+        public IQueryable<T> GetQueryable(Expression<Func<T, bool>>? filter = null, IEnumerable<Expression<Func<T, object>>>? includes = null, bool asNoTracking = false)
+        {
+            var dbset = _dbSet.AsQueryable();
+
+            if (filter != null)
+            {
+                dbset = dbset.Where(filter);
+            }
+
+            if (includes != null)
+            {
+                foreach (var include in includes)
+                {
+                    dbset = dbset.Include(include);
+                }
+            }
+
+            if (asNoTracking)
+            {
+                dbset = dbset.AsNoTracking();
+            }
+
+            return dbset;
+        }
+
+        public Task RemoveAsync(T entity)
+        {
+            _dbSet.Remove(entity);
+            return Task.CompletedTask;
+        }
+
+        public Task<T> UpdateAsync(T entity)
+        {
+            return Task.FromResult(Update(entity));
+        }
+
+        private T Update(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            return entity;
+        }
+    }
+}
